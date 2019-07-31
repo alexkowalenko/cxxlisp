cc6c91dab5b840c3db2de0f6151f251bf8dfa9d5
Alex Kowalenko <alex.kowalenko@gmail.com>
2019-07-31 16:50:38 +0100

    Initial structure

---
+++ cmd/cxxlisp.cc
@@ -0,0 +1,15 @@
+//
+// Common Lisp in C++17
+//
+// Copyright © Alex Kowalenko 2019.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello C++ Lisp 👾 !" << endl;
+
+    return 0;
+}
